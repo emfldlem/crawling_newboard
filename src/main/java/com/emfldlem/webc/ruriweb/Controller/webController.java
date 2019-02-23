531a6d3a753b7876e53f7e16dc7714b967397163
swkim <eiwak@naver.com>
2019-02-23 17:54:33 +0900

    2019-02-23

--- src/main/java/com/emfldlem/webc/ruriweb/Controller/webController.java
+++ src/main/java/com/emfldlem/webc/ruriweb/Controller/webController.java
@@ -18,6 +18,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
 
+import static com.emfldlem.webc.ruriweb.Common.CommonUtil.readFileId;
+
 @Controller
 public class webController {
 
@@ -64,7 +66,7 @@ public class webController {
             for (Element anElem : elem) {
                 /*새로운 json 객체에 각 각의 요소를 담기*/
                 JSONObject tempJsonObject = new JSONObject();
-                int lastId = CommonUtil.readFileId(file);
+                int lastId = readFileId(file);
                 String subject = anElem.select(".deco").text();
                 String link = anElem.select(".deco").attr("href");
                 int sid = Integer.parseInt(anElem.text().split(" ")[0]);
@@ -83,6 +85,55 @@ public class webController {
         }
 
         finalJsonObject.put("ruriweb", jsonArray);
+        System.out.println(finalJsonObject);
+        return finalJsonObject;
+    }
+
+    @PostMapping("ppomppuDataCall")
+    public @ResponseBody
+    JSONObject ppomppuDataCall() {
+        /* json 객체를 담을 json 배열  */
+        JSONArray jsonArray = new JSONArray();
+        /* 최종 객체를 담을 json */
+        JSONObject finalJsonObject = new JSONObject();
+
+        try {
+            //TODO 추후 DB가 구축된다면 DB에서 값을 가져오자
+            File file = new File("C:/dev/txt/ppomppu.txt");
+            String URL = "http://www.ppomppu.co.kr/zboard/zboard.php?id=ppomppu";
+            Document doc = Jsoup.connect(URL).get();
+            Elements elem = doc.select(".list_title");
+            Collections.reverse(elem);
+
+            for (Element anElem : elem) {
+                /*새로운 JSON 객체 */
+                JSONObject tempJsonObject = new JSONObject();
+
+                int lastId = readFileId(file);
+
+                Element elem1 = anElem.parent();
+                Element elem2 = anElem.parent().parent().parent().parent().parent().parent().parent().child(0);
+
+                String subject = elem1.text();
+                String link = "http://www.ppomppu.co.kr/zboard/" + elem1.attr("href");
+                String No = elem2.text();
+
+                int sid = Integer.parseInt(No);
+                if (sid > lastId) {
+                    System.out.println("sid =========" + sid);
+                    System.out.println("subject =========" + subject);
+                    tempJsonObject.put("sid", sid);
+                    tempJsonObject.put("subject", subject);
+                    tempJsonObject.put("link", link);
+                    jsonArray.add(tempJsonObject);
+                    //creatFileId(sid,file);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        finalJsonObject.put("ppompu", jsonArray);
         System.out.println(finalJsonObject);
         return finalJsonObject;
 
