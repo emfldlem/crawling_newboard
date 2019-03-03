470354b7e47be884b06f028cfeb2dc2f8a5aa52e
swkim <eiwak@naver.com>
2019-03-03 18:42:06 +0900

    2019-03-03

--- src/main/java/com/emfldlem/webc/ruriweb/Controller/webController.java
+++ src/main/java/com/emfldlem/webc/ruriweb/Controller/webController.java
@@ -1,6 +1,6 @@
 package com.emfldlem.webc.ruriweb.Controller;
 
-import com.emfldlem.webc.ruriweb.Common.CommonUtil;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.jsoup.Jsoup;
@@ -8,17 +8,17 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.io.File;
+
 import java.io.IOException;
 import java.util.Collections;
 
-import static com.emfldlem.webc.ruriweb.Common.CommonUtil.readFileId;
+
 
 @Controller
 public class webController {
@@ -46,7 +46,7 @@ public class webController {
         JSONObject finalJsonObject = new JSONObject();
 
         try {
-            File file = new File("C:/dev/txt/ruriweb_hotdeal.txt");
+           // File file = new File("C:/dev/txt/ruriweb_hotdeal.txt");
             String URL = "http://bbs.ruliweb.com/ps/board/1020";
             Document doc = null;
             doc = Jsoup.connect(URL).get();
@@ -60,25 +60,27 @@ public class webController {
             }
 
             /*최신 글 상위 5개 가져옴*/
-            /*Collections.reverse(elem);
-            elem.subList(0, 25).clear();*/
+            Collections.reverse(elem);
+            elem.subList(0, 3).clear();
 
             for (Element anElem : elem) {
                 /*새로운 json 객체에 각 각의 요소를 담기*/
                 JSONObject tempJsonObject = new JSONObject();
-                int lastId = readFileId(file);
+                //int lastId = readFileId(file);
                 String subject = anElem.select(".deco").text();
                 String link = anElem.select(".deco").attr("href");
                 int sid = Integer.parseInt(anElem.text().split(" ")[0]);
-                if (sid > lastId) {
-                    System.out.println("sid =========" + sid);
-                    System.out.println("subject =========" + subject);
-                    tempJsonObject.put("sid", sid);
-                    tempJsonObject.put("subject", subject);
-                    tempJsonObject.put("link", link);
-                    jsonArray.add(tempJsonObject);
 
-                }
+                System.out.println("sid =========" + sid);
+                System.out.println("subject =========" + subject);
+                tempJsonObject.put("sid", sid);
+                tempJsonObject.put("subject", subject);
+                tempJsonObject.put("link", link);
+                jsonArray.add(tempJsonObject);
+
+                /*if (sid > lastId) {
+
+                }*/
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -99,7 +101,7 @@ public class webController {
 
         try {
             //TODO 추후 DB가 구축된다면 DB에서 값을 가져오자
-            File file = new File("C:/dev/txt/ppomppu.txt");
+            //File file = new File("C:/dev/txt/ppomppu.txt");
             String URL = "http://www.ppomppu.co.kr/zboard/zboard.php?id=ppomppu";
             Document doc = Jsoup.connect(URL).get();
             Elements elem = doc.select(".list_title");
@@ -109,7 +111,7 @@ public class webController {
                 /*새로운 JSON 객체 */
                 JSONObject tempJsonObject = new JSONObject();
 
-                int lastId = readFileId(file);
+               // int lastId = readFileId(file);
 
                 Element elem1 = anElem.parent();
                 Element elem2 = anElem.parent().parent().parent().parent().parent().parent().parent().child(0);
@@ -118,8 +120,17 @@ public class webController {
                 String link = "http://www.ppomppu.co.kr/zboard/" + elem1.attr("href");
                 String No = elem2.text();
 
+
                 int sid = Integer.parseInt(No);
-                if (sid > lastId) {
+
+                System.out.println("sid =========" + sid);
+                System.out.println("subject =========" + subject);
+                tempJsonObject.put("sid", sid);
+                tempJsonObject.put("subject", subject);
+                tempJsonObject.put("link", link);
+                jsonArray.add(tempJsonObject);
+
+                /*if (sid > lastId) {
                     System.out.println("sid =========" + sid);
                     System.out.println("subject =========" + subject);
                     tempJsonObject.put("sid", sid);
@@ -127,7 +138,7 @@ public class webController {
                     tempJsonObject.put("link", link);
                     jsonArray.add(tempJsonObject);
                     //creatFileId(sid,file);
-                }
+                }*/
             }
         } catch (IOException e) {
             e.printStackTrace();
